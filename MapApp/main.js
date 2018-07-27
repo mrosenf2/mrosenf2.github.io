66b497c0f50f00e1e8f8560ef4b8a8c07aabf86c
mrosenf2 <matthew.rosenfeld22@gmail.com>
2018-07-27 14:27:40 -0400

    many edits

--- MapApp/main.js
+++ MapApp/main.js
@@ -1,42 +1,51 @@
 var map;
-
+function geturl() {
+  var largeImage = document.getElementById('image');
+  var url=largeImage.getAttribute('src');
+  return url
+}
 function initMap() {
   // console.log(fs.readdir())
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 17.031, lng: 103.079},
-    zoom: 5
+    center: {lat: 15.9757, lng: 102.6331},
+    zoom: 6
   });
 
   //get marker data from server
 
   // loop through json files in folder
 
-
-
-  data = jQuery.getJSON('MapApp/data/Asia/IMG_20180711_183536.jpg.json', function(data) {
-    console.log(data)
+  data = jQuery.getJSON('data/Asia/IMG_20180711_183536.jpg.json', function(data) {
     var pic = data.geoData
-    var marker = new google.maps.Marker({position: {lat: pic.latitude, lng: pic.longitude}, map: map})
-    marker.addListener('mouseover', function(event) {
-
-      var x = event.va.clientX;
-      var y = event.va.clientY;
-      var img = document.createElement("img");
-      var src = document.getElementById("map");
-      img.src = 'MapApp/data/Asia/IMG_20180711_183536.jpg' //need to modify later
-      img.id = "pic"
-      img.style.display = '';
-      img.style.position = 'absolute';
-      img.style.left = x + 'px';
-      img.style.top = y + 'px';
-      img.style.width = '10%';
-      src.appendChild(img)
-
+    var datetime = data.photoTakenTime.formatted
+    var path = window.location.origin + '/MapApp/data/Asia/IMG_20180711_183536.jpg'
+    imgstr = "<a href=\'" + path + "\' target=\"_blank\">  <img src=\'data/Asia/IMG_20180711_183536.jpg\' " + "style=\'height: 250px\'" + "/> </a>"
+    contentstr = "<div style=\'margin: 0px\'> " + imgstr + "<p> " + datetime + " </p>" + " </div>"
+    var marker = new google.maps.Marker(
+      {position: {lat: pic.latitude, lng: pic.longitude},
+      map: map,
+      title: 'click to view image full size'
     })
-    marker.addListener('mouseout', function(event) {
-      img = document.getElementById("pic");
-      src = document.getElementById("map");
-      src.removeChild(img)
+    var infowindow = new google.maps.InfoWindow({
+      content: contentstr,
+      maxwidth: 250
+    });
+    marker.addListener('mouseover', function() {
+      infowindow.open(map, marker)
+    })
+    marker.addListener('click', function() {
+      window.open(path, '_blank')
+      infowindow.close()
+    })
+    marker.addListener('mouseout', function() {
+      console.log('out');
+      tm = setTimeout(function() {
+        infowindow.close()
+      }, 1000)
+      marker.addListener('mouseover', function() {
+        marker.remove
+        clearTimeout(tm)
+      })
     })
 
   });
