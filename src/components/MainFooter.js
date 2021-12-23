45c8e98b272b300bdc8ee54f6cea1a4dff131c4c
Matthew <matthew.rosenfeld22@gmail.com>
2021-12-22 19:17:41 -0500

    Updates

--- src/components/MainFooter.js
+++
@@ -1,7 +0,0 @@
-import './MainFooter.css';
-
-export default function MainFooter() {
-    return <footer className="footer">
-        <p>created and maintained by Matthew Rosenfeld</p>
-    </footer>;
-}
